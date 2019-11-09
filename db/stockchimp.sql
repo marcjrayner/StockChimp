6f77a5882cc0adead79c28568b94ad77cbf873a0
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-09 11:25:27 +0000

    creates items table on db sql file

--- db/stockchimp.sql
+++ db/stockchimp.sql
@@ -9,3 +9,18 @@ CREATE TABLE suppliers(
   contact_job_title VARCHAR(255)
   contact_phone_number VARCHAR(255),
 )
+
+CREATE TABLE items
+(
+  id SERIAL8 PRIMARY KEY,
+  part_number VARCHAR(255),
+  manufacturer VARCHAR(255),
+  category VARCHAR(255),
+  condition VARCHAR(255),
+  status VARCHAR(255),
+  quantity INT4,
+  supplier_id INT8 REFERENCES suppliers(id)
+  unit_buy_price MONEY,
+  target_unit_sell_price MONEY,
+  minimum_unit_sell_price MONEY,
+);
