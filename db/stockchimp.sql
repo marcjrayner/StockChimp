9cd5a64a894a1f31029de380cfad5cae2615e71a
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-09 11:25:03 +0000

    creates suppliers table on db sql file

--- db/stockchimp.sql
+++ db/stockchimp.sql
@@ -1,2 +1,11 @@
 DROP TABLE items;
 DROP TABLE suppliers;
+
+CREATE TABLE suppliers(
+  id SERIAL8 PRIMARY KEY,
+  company_name VARCHAR(255),
+  address VARCHAR(255),
+  contact_name VARCHAR(255),
+  contact_job_title VARCHAR(255)
+  contact_phone_number VARCHAR(255),
+)
