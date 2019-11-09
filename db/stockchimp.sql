7011c62bba00de29bb7f83b88f17e1ab6c0bfd86
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-09 11:31:03 +0000

    changes to db file, sql_runner file db name corrected

--- db/stockchimp.sql
+++ db/stockchimp.sql
@@ -1,14 +1,14 @@
-DROP TABLE items;
-DROP TABLE suppliers;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS suppliers;
 
 CREATE TABLE suppliers(
   id SERIAL8 PRIMARY KEY,
   company_name VARCHAR(255),
   address VARCHAR(255),
   contact_name VARCHAR(255),
-  contact_job_title VARCHAR(255)
-  contact_phone_number VARCHAR(255),
-)
+  contact_job_title VARCHAR(255),
+  contact_phone_number VARCHAR(255)
+);
 
 CREATE TABLE items
 (
@@ -19,8 +19,8 @@ CREATE TABLE items
   condition VARCHAR(255),
   status VARCHAR(255),
   quantity INT4,
-  supplier_id INT8 REFERENCES suppliers(id)
+  supplier_id INT8 REFERENCES suppliers(id),
   unit_buy_price MONEY,
-  target_unit_sell_price MONEY,
-  minimum_unit_sell_price MONEY,
+  target_sell_price MONEY,
+  min_sell_price MONEY
 );
