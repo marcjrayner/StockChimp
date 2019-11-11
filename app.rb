bf40e07e9b5a09c8f9e048424be1bf0593917b6a
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-11 20:44:01 +0000

    adds link to supplier page from item show

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/items_controller')
 require_relative('controllers/suppliers_controller')
+also_reload( 'models/*' )
+
 
 get '/' do
   erb(:index)
